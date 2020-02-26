b207f2a9640a135cde52e6ca60310484274a000f
Henrik Ilgen <henrik@henrik-ilgen.de>
2020-02-26 14:55:11 +0100

    Fixed Spaces/Tabs

--- total-hours-worked.primaerp.user.js
+++ total-hours-worked.primaerp.user.js
@@ -8,10 +8,10 @@
 // ==/UserScript==
 
 (function() {
-  function updateTotalTime() {
-  	const sum = [...document.querySelectorAll('#week_time_chart text')].map(x => x.innerHTML).filter(x => !isNaN(x)).reduce((x, y) => +x + +y);
+	function updateTotalTime() {
+  		const sum = [...document.querySelectorAll('#week_time_chart text')].map(x => x.innerHTML).filter(x => !isNaN(x)).reduce((x, y) => +x + +y);
 		document.querySelector('#week-chart .desktop-panel-heading h2').innerHTML = `Week statistics (${sum}h)`;
-  }
+	}
   
-  window.setInterval(updateTotalTime, 1000);
+	window.setInterval(updateTotalTime, 1000);
 })();
