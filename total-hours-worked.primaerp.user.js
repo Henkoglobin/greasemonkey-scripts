f04b70730e36b59a1a75108a137a170df88d296c
Henrik Ilgen <henrik@henrik-ilgen.de>
2020-02-26 14:32:35 +0100

    [primaerp] Script for total hours per week

---
+++ total-hours-worked.primaerp.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        primaERP - Total Hours Worked
+// @description Displays the total time worked in any week.
+// @author      Henrik Ilgen, https://github.com/henkoglobin
+// @version     0.0.1
+// @grant       none
+// @match       https://*.primaerp.com/
+// ==/UserScript==
+
+(function() {
+  function updateTotalTime() {
+  	const sum = [...document.querySelectorAll('#week_time_chart text')].map(x => x.innerHTML).filter(x => !isNaN(x)).reduce((x, y) => +x + +y);
+		document.querySelector('#week-chart .desktop-panel-heading h2').innerHTML = `Week statistics (${sum}h)`;
+  }
+  
+  window.setInterval(updateTotalTime, 1000);
+})();
