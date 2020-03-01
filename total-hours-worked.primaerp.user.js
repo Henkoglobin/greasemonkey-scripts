c62fe998b4cbff4a37073dc321d9a3cdbfb4548f
Johannes <johannesfeige@gmail.com>
2020-03-01 15:50:49 +0100

    removed logs

--- total-hours-worked.primaerp.user.js
+++ total-hours-worked.primaerp.user.js
@@ -14,17 +14,14 @@
         }
         const sum = window.weekChartData.sum((x) => x.value);
         document.querySelector('#week-chart .desktop-panel-heading h2').innerHTML = `Week statistics (${sum}h)`;
-        console.info('Week total set to', sum);
+
         return true;
     }
 
     function createWeekTimeChartObserver() {
         const weekTimeChart = document.getElementById('week_time_chart');
         const options = { childList: true };
-        const observer = new MutationObserver(() => {
-            console.log('--observer--');
-            updateTotalTime();
-        });
+        const observer = new MutationObserver(() => updateTotalTime());
 
         observer.observe(weekTimeChart, options);
 
