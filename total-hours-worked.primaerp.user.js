c19d5bc161350d77cbdbb8d4f4c3672dca3f3619
Johannes <johannesfeige@gmail.com>
2020-03-07 15:17:28 +0100

    introduced classes

--- total-hours-worked.primaerp.user.js
+++ total-hours-worked.primaerp.user.js
@@ -5,119 +5,112 @@
 // @version     0.0.4
 // @grant       none
 // @match       https://*.primaerp.com/
+// @require     https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.5.4/rxjs.umd.js
 // ==/UserScript==
 
-(function() {
-    const WEEK_STATISTICS_SELECTOR = '#week-chart .desktop-panel-heading h2';
-    const WEEK_CHART_ID = 'week-chart';
-    const WEEK_TIME_CHART_ID = 'week_time_chart';
-    const WEEK_REPORT_REQUEST_URL = '/reports/ajaxWeekTimeReport';
+const WEEK_STATISTICS_SELECTOR = '#week-chart .desktop-panel-heading h2';
+const WEEK_CHART_ID = 'week-chart';
+const WEEK_TIME_CHART_ID = 'week_time_chart';
+const WEEK_REPORT_REQUEST_URL = '/reports/ajaxWeekTimeReport';
 
-    const MONTH_DEV_PREFIX = 'jofeHeil-month-';
+const MONTH_DEV_PREFIX = 'jofeHeil-month-';
 
-    const DAILY_WORK_HOURS = 8;
-    const MONTH_COUNT = 2;
-    const START_OF_MONTHS = (() => {
-        const result = [];
+const DAILY_WORK_HOURS = 8;
+const MONTH_COUNT = 2;
+const START_OF_MONTHS = (() => {
+    const result = [];
 
-        for (let index = 0; index < MONTH_COUNT; index++) {
-            result.push(
-                moment()
-                    .startOf('month')
-                    .subtract(index, 'months')
-            );
+    for (let index = 0; index < MONTH_COUNT; index++) {
+        result.push(
+            moment()
+                .startOf('month')
+                .subtract(index, 'months')
+        );
+    }
+
+    return result;
+})();
+
+class TranslationService {
+    fixCurrentWeekTranslation() {
+        const weektime = window.messages.content.dashboard.panels.weektime;
+        if (weektime.actual() == 'Actual week') {
+            weektime.actual = function() {
+                return 'Current week';
+            };
         }
+    }
+}
 
-        return result;
-    })();
-
-    const helper = getHelper();
-
-    function updateTotalTime() {
-        if (!window.weekChartData) {
-            return false;
-        }
-        const sum = window.weekChartData.sum((x) => x.value);
-        document.querySelector(WEEK_STATISTICS_SELECTOR).innerHTML = `Week statistics (${sum}h)`;
-
-        return true;
+class AdditionalTimesService {
+    constructor() {
+        this._helper = (() => {
+            const getMonthDivId = (month) => `${MONTH_DEV_PREFIX}${month.year()}-${month.month()}`;
+            return {
+                getMonthDivId,
+                getMonthDivComparisonId: (month) => `${getMonthDivId(month)}-comparison`,
+                getNormalizedDayOfWeek: (momentDate) => {
+                    const weekday = momentDate.weekday();
+                    return weekday === 0 ? 6 : weekday - 1;
+                },
+                getDateTargetHours: (momentDate) =>
+                    momentDate.weekday() > 0 && momentDate.weekday() < 6 ? DAILY_WORK_HOURS : 0,
+            };
+        })();
     }
 
-    function updateMonthTimes() {
-        START_OF_MONTHS.forEach((month) => {
-            getTimesPerMonth(month).then((data) => updateTimesPerMonth(data, month));
-        });
+    init() {
+        this._createWeekTimeChartObserver();
+        this._initMonths();
     }
 
-    function updateTimes() {
-        updateTotalTime();
-        updateMonthTimes();
-    }
-
-    function createWeekTimeChartObserver() {
-        const weekTimeChart = document.getElementById(WEEK_TIME_CHART_ID);
-        const options = { childList: true };
-        const observer = new MutationObserver(() => updateTimes());
-
-        observer.observe(weekTimeChart, options);
-
-        return observer;
-    }
-
-    function initMonths() {
+    _initMonths = () => {
         START_OF_MONTHS.forEach((month) => {
             const $parent = $(`#${WEEK_CHART_ID}`).parent();
 
-            const $monthDiv = $('<div>', { class: 'desktop-panel', id: helper.getMonthDivId(month) }).appendTo($parent);
+            const $monthDiv = $('<div>', { class: 'desktop-panel', id: this._helper.getMonthDivId(month) }).appendTo(
+                $parent
+            );
             const $heading = $('<div>', { class: 'desktop-panel-heading' }).appendTo($monthDiv);
             $('<h2>')
                 .text(`${month.format('MMMM YYYY')}`)
                 .appendTo($heading);
-            $('<div>', { class: 'desktop-panel-body', id: helper.getMonthDivComparisonId(month) }).appendTo($monthDiv);
+            $('<div>', { class: 'desktop-panel-body', id: this._helper.getMonthDivComparisonId(month) }).appendTo(
+                $monthDiv
+            );
         });
-    }
-    
-    function fixCurrentWeekTranslation() {
-        const weektime = window.messages.content.dashboard.panels.weektime;
-        if(weektime.actual() == "Actual week") {
-            weektime.actual = function() { return "Current week"; };
-        }   
-    }
+    };
 
-    /**
-     * Preparation of months because ajayWeekTimeReport return whole week
-     * (also including Days of previous or following months)
-     * Also enchric target per day and moment date
-     * @param {Array} data
-     * @param {Date} startOfMonth
-     * @param {Date} endOfMonth
-     * @returns {Array} processed Data
-     */
-    function primaMonthPreparation(data, startOfMonth, endOfMonth) {
-        const startOfMonthWeekDay = helper.getNormalizedDayOfWeek(startOfMonth);
-        const endOfMonthWeekDay = helper.getNormalizedDayOfWeek(endOfMonth);
+    _createWeekTimeChartObserver = () => {
+        const weekTimeChart = document.getElementById(WEEK_TIME_CHART_ID);
+        const options = { childList: true };
+        const observer = new MutationObserver(this._updateTimes);
 
-        if (startOfMonthWeekDay > 0) {
-            data = data.slice(startOfMonthWeekDay, data.length);
+        observer.observe(weekTimeChart, options);
+
+        return observer;
+    };
+
+    _updateTimes = () => {
+        this._updateTotalTime();
+        this._updateMonthTimes();
+    };
+
+    _updateTotalTime = () => {
+        if (!window.weekChartData) {
+            return;
         }
+        const sum = window.weekChartData.sum((x) => x.value);
+        document.querySelector(WEEK_STATISTICS_SELECTOR).innerHTML = `Week statistics (${sum}h)`;
+    };
 
-        if (endOfMonthWeekDay < 6) {
-            const carryover = 6 - endOfMonthWeekDay;
-            data.splice(-carryover, carryover);
-        }
-
-        let dateCounter = 0;
-        data.forEach((item) => {
-            item.momentDate = startOfMonth.clone().add(dateCounter, 'day');
-            item.targetHours = helper.getDateTargetHours(item.momentDate);
-            item.balance = item.value - item.targetHours;
-            dateCounter++;
+    _updateMonthTimes = () => {
+        START_OF_MONTHS.forEach((month) => {
+            this._getTimesPerMonth(month).then((data) => this._updateTimesPerMonth(data, month));
         });
+    };
 
-        return data;
-    }
-
-    function getTimesPerMonth(startOfMonth) {
+    _getTimesPerMonth = (startOfMonth) => {
         const endOfMonth = moment(startOfMonth).endOf('month');
 
         const startOfWeeks = [];
@@ -152,10 +145,43 @@
         return Promise.all(requests.map((x) => fetch(x.request, x.init)))
             .then((responses) => Promise.all(responses.map((response) => response.json())))
             .then((data) => data.flat(1))
-            .then((data) => primaMonthPreparation(data, startOfMonth, endOfMonth));
-    }
+            .then((data) => this._primaMonthPreparation(data, startOfMonth, endOfMonth));
+    };
 
-    function updateTimesPerMonth(data, startOfMonth) {
+    /**
+     * Preparation of months because ajayWeekTimeReport return whole week
+     * (also including Days of previous or following months)
+     * Also enchric target per day and moment date
+     * @param {Array} data
+     * @param {Date} startOfMonth
+     * @param {Date} endOfMonth
+     * @returns {Array} processed Data
+     */
+    _primaMonthPreparation = (data, startOfMonth, endOfMonth) => {
+        const startOfMonthWeekDay = this._helper.getNormalizedDayOfWeek(startOfMonth);
+        const endOfMonthWeekDay = this._helper.getNormalizedDayOfWeek(endOfMonth);
+
+        if (startOfMonthWeekDay > 0) {
+            data = data.slice(startOfMonthWeekDay, data.length);
+        }
+
+        if (endOfMonthWeekDay < 6) {
+            const carryover = 6 - endOfMonthWeekDay;
+            data.splice(-carryover, carryover);
+        }
+
+        let dateCounter = 0;
+        data.forEach((item) => {
+            item.momentDate = startOfMonth.clone().add(dateCounter, 'day');
+            item.targetHours = this._helper.getDateTargetHours(item.momentDate);
+            item.balance = item.value - item.targetHours;
+            dateCounter++;
+        });
+
+        return data;
+    };
+
+    _updateTimesPerMonth = (data, startOfMonth) => {
         const times = [
             {
                 title: 'Actual',
@@ -171,32 +197,18 @@
             },
         ];
 
-        console.log({ data });
-
-        const $comparisonDiv = $(`#${helper.getMonthDivComparisonId(startOfMonth)}`);
+        const $comparisonDiv = $(`#${this._helper.getMonthDivComparisonId(startOfMonth)}`);
         $comparisonDiv.empty();
 
         times.forEach((time) => {
             const $paragraph = $('<p>').appendTo($comparisonDiv);
             $paragraph.html(`${time.title}: ${time.value}`);
         });
-    }
+    };
+}
 
-    createWeekTimeChartObserver();
-    initMonths();
-    fixCurrentWeekTranslation();
-
-    function getHelper() {
-        const getMonthDivId = (month) => `${MONTH_DEV_PREFIX}${month.year()}-${month.month()}`;
-        return {
-            getMonthDivId,
-            getMonthDivComparisonId: (month) => `${getMonthDivId(month)}-comparison`,
-            getNormalizedDayOfWeek: (momentDate) => {
-                let weekday = momentDate.weekday();
-                return weekday === 0 ? 6 : weekday - 1;
-            },
-            getDateTargetHours: (momentDate) =>
-                momentDate.weekday() > 0 && momentDate.weekday() < 6 ? DAILY_WORK_HOURS : 0,
-        };
-    }
-})();
+(function(translationService, timesService) {
+    translationService.fixCurrentWeekTranslation();
+    timesService._createWeekTimeChartObserver();
+    timesService._initMonths();
+})(new TranslationService(), new AdditionalTimesService());
